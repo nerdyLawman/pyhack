7fe8620ffef19e491ff567628683665ef0305197
nerdylawman <andrewtlyman@gmail.com>
2016-08-23 20:46:03 -0500

    quickfixed globals and added fullscreen

--- src/pyhackrl.py
+++ src/pyhackrl.py
@@ -3,6 +3,42 @@ import libtcodpy as libtcod
 import math
 import textwrap
 
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 50
+LIMIT_FPS = 20
+
+BAR_WIDTH = 20
+PANEL_HEIGHT = 7
+PANEL_Y = SCREEN_HEIGHT - PANEL_HEIGHT
+
+MSG_X = BAR_WIDTH + 2
+MSG_WIDTH = SCREEN_WIDTH - BAR_WIDTH - 2
+MSG_HEIGHT = PANEL_HEIGHT - 1
+
+INVENTORY_WIDTH = 50
+
+HEAL_AMOUNT = 4
+
+MAP_WIDTH = 80
+MAP_HEIGHT = 43
+
+ROOM_MAX_SIZE = 10
+ROOM_MIN_SIZE = 6
+MAX_ROOMS = 30
+
+MAX_ROOM_MONSTERS = 3
+MAX_ROOM_ITEMS = 2
+
+FOV_ALGO = 2
+FOV_LIGHT_WALLS = True
+TORCH_RADIUS = 8
+
+color_dark_wall = libtcod.Color(0, 0, 100)
+color_light_wall = libtcod.Color(130, 110, 50)
+color_dark_ground = libtcod.Color(50, 50, 100)
+color_light_ground = libtcod.Color(200, 180, 50)
+
+
 class Tile:
     # map tile and properties
     def __init__(self, blocked, block_sight = None):
@@ -494,6 +530,8 @@ def handle_keys():
         	chosen_item = inventory_menu('Press the key next to an item to use it, or any other to cancel.\n')
         	if chosen_item is not None:
         	    chosen_item.use()
+        if key_char == 'f':
+            libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
 
         return('no turn')
 
